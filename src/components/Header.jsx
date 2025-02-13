bdf408f2cadf8a2f4dbbc04219a5fdc525ef9399
jamesdai05 <dai_guilong@hotmail.com>
2025-02-13 13:23:06 +0800

    completed the css of navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,31 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+
 
 const Header = () => {
+  const fsStyle={color:"#fff"}
   return (
     <Navbar expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="#home">E-shop</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="d-flex justify-content-end"
+        >
+          {/* <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
+          </Nav> */}
+          <Nav className="ms-auto">
+            <Nav.Link href="/cart" style={fsStyle}>
+              Cart <FaShoppingCart className="ms-0.5" />
+            </Nav.Link>
+            <Nav.Link href="#link" style={fsStyle}>
+              User <FaUser className="ms-0.5" />
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
