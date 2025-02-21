a3b09e7baf2a3fa78a1b6f63fb3efcabcc3b4a88
jamesdai05 <dai_guilong@hotmail.com>
2025-02-22 01:25:42 +0800

    set up the the store and slice for the reduxtoolkit

--- src/store.js
+++ src/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./Slices/apiSlice.js";
 
 const store=configureStore({
-  reducer:{}
+  reducer:{
+    [apiSlice.reducerPath]:apiSlice.reducer,
+  },
+  middleWare:(getDefaultMiddleWare)=>getDefaultMiddleWare().concat(apiSlice.middleware),
+  devTools:true,
 })
 
 
