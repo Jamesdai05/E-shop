6a84f5fe7ee7a0238e779d76a4f0409646f97afb
jamesdai05 <dai_guilong@hotmail.com>
2025-02-25 19:52:21 +0800

    fixed the error in login

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -8,28 +8,40 @@ import { setCredentials } from "../Slices/authSlice.js";
 import { useLoginMutation } from "../Slices/userSlice.js";
 import { toast } from "react-toastify";
 
-
-
-
 const LoginPage = () => {
-  const [email,setEmail]=useState("");
-  const [password,setPassword]=useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmit=(e)=>{
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const { search } = useLocation();
+  const searchPara = new URLSearchParams(search);
+  // retrieve the query parameter redirect and fallback value
+  const redirect = searchPara.get("redirect") || "/";
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [userInfo, redirect, navigate]);
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password)
-  }
-
+    console.log(email, password);
+  };
 
   return (
     <FormContainer>
       <h1>Sign In</h1>
 
       <Form onSubmit={handleSubmit}>
-        <Form.Group>
-          <Form.Label controlid="email" className="my-2">
-            Email Address:
-          </Form.Label>
+        <Form.Group controlId="email" className="my-2">
+          <Form.Label>Email:</Form.Label>
           <Form.Control
             type="email"
             placeholder="enter your email"
@@ -37,10 +49,8 @@ const LoginPage = () => {
             onChange={(e) => setEmail(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        <Form.Group>
-          <Form.Label controlid="password" className="my-2">
-            Password:
-          </Form.Label>
+        <Form.Group controlId="password" className="my-2">
+          <Form.Label>Password:</Form.Label>
           <Form.Control
             type="password"
             placeholder="enter your password"
@@ -48,15 +58,20 @@ const LoginPage = () => {
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        <Button type="submit" variant="primary" className="btn btn-lg my-4">Log In</Button>
+        <Button type="submit" variant="primary" className="btn btn-lg my-4">
+          Log In
+        </Button>
       </Form>
 
       <Row className="py-3">
         <Col>
-          New user ? <Link className="fw-bold" to="/register">Sign Up</Link>
+          New user ?{" "}
+          <Link className="fw-bold" to="/register">
+            Sign Up
+          </Link>
         </Col>
       </Row>
     </FormContainer>
   );
-}
-export default LoginPage
+};
+export default LoginPage;
